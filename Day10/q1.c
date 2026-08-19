65152e960235dd4629283e6150fecf9d8b389121
himanshusingh1022 <himanshusingh43366@gmail.com>
2026-08-19 14:54:42 +0530

    day10

---
+++ Day10/q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int side1, side2, side3;
+
+	scanf("%d %d %d", &side1, &side2, &side3);
+
+	if (side1 == side2 && side2 == side3)
+	{
+		printf("Equilateral");
+	}
+	else if (side1 == side2 || side2 == side3 || side1 == side3)
+	{
+		printf("Isosceles");
+	}
+	else
+	{
+		printf("Scalene");
+	}
+
+	return 0;
+}
