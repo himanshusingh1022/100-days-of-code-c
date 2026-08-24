d994d6eab86f7a26774c3a089cadd6cc480555f1
himanshusingh1022 <himanshusingh43366@gmail.com>
2026-08-24 15:00:34 +0530

    day15

---
+++ Day15/q2.c
@@ -0,0 +1,20 @@
+/* Q30: Reverse a given number. */
+#include <stdio.h>
+
+int main(void)
+{
+	int number;
+	int reversed = 0;
+
+	scanf("%d", &number);
+
+	while (number != 0)
+	{
+		reversed = reversed * 10 + number % 10;
+		number /= 10;
+	}
+
+	printf("%d", reversed);
+
+	return 0;
+}
